aa75fbb9734259c71d6a232ef9215fdf06032869
이동준 <bomul1128@naver.com>
2022-04-10 22:51:44 +0900

    create.html 레이아웃 작성

--- plans/models.py
+++ plans/models.py
@@ -6,14 +6,22 @@ class Plan(models.Model):
 
     # 약속 제목
     title = models.CharField(max_length=20)
+
     # 약속 날짜
     date = models.DateField()
+
+    # 약속 시간
+    time = models.TimeField()
+
     # 약속 최대 인원
     people_limit = models.IntegerField() 
+
     # 약속 장소
     place = models.CharField(max_length=20)
+
     # 약속 설명
     description = models.TextField()
+
 
     def __str__(self) -> str:
         return super().__str__()
