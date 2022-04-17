1383c34cedebba9293ab915684834edcb0c9fe85
이동준 <bomul1128@naver.com>
2022-04-17 22:32:37 +0900

    로그인/로그아웃 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = "accounts"
 urlpatterns = [
     path("signup/", views.signup, name="signup"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
-from django.views.decorators.http import require_http_methods
+from django.views.decorators.http import require_http_methods, require_POST
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
 
 
 @require_http_methods(["POST", "GET"])
@@ -23,4 +25,26 @@ def signup(request):
     return render(request, "accounts/signup.html", context)
 
 
-# Create your views here.
+@require_http_methods(['POST', 'GET'])
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('plans:index')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get('next') or 'plans:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+@require_POST
+def logout(request):
+    if request.user.is_authenticated:
+        auth_logout(request)
+    return redirect('plans:index')
