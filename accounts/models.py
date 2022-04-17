a5beca695a9269ec87f99ad7a8d7114183d101ea
Hyelim <hallov012@gmail.com>
2022-04-18 00:30:17 +0900

    accounts/models.py User 정보 수정

--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=10)
